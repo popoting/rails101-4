e9a9d59868f97a6b1cf51f80408771c492f6169d
你的名字 <luonayin@gmail.com>
2017-01-05 14:26:13 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,14 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destrou]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
-  
+
   def index
     @groups = Group.all
   end
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
